6f05be6e6893ef88b5e7d8155e911a043e67cc1d
Scott Fox <scottfox87@gmail.com>
2018-08-29 10:36:41 -0500

    basic skeleton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Layout from "./hoc/Layout/Layout";
 
 class App extends Component {
   render() {
     return (
       <div>
-
+        <Layout>
+            Most Popular By Language
+            Most Popular Overall
+        </Layout>
       </div>
     );
   }
---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/Aux/Aux';
+import classes from './Layout.css';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+
+class Layout extends Component {
+
+    render () {
+        return (
+            <Aux>
+                <Toolbar />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+
+        );
+    }
+}
+
+export default Layout;
