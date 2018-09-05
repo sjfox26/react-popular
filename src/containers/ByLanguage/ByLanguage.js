b36fa3acb7f6c3924fe2dcd98440a96524877049
Scott Fox <scottfox87@gmail.com>
2018-09-05 15:31:40 -0500

    console log data from api to test connection

--- src/containers/ByLanguage/ByLanguage.js
+++ src/containers/ByLanguage/ByLanguage.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import Aux from '../../hoc/Aux/Aux';
 import classes from "./ByLanguage.css";
-
+import axios from 'axios';
 
 
 class ByLanguage extends Component {
 
     state = {
-        test: 'HI'
+        test: 'HI',
+        language: 'all',
+        languages: ['all', 'javascript', 'ruby', 'python']
+    }
+
+    componentDidMount () {
+        const url = `https://api.github.com/search/repositories?q=stars:>1+language:${this.state.language}&sort=stars&order=desc&type=Repositories`;
+        axios.get(url)
+            .then(response => {
+               console.log(response);
+            })
     }
 
     render () {
@@ -17,8 +27,7 @@ class ByLanguage extends Component {
             <Aux>
                 <div className={classes.ByLanguage}>
                     <div>
-                        Languages
-                        {this.state.test}
+
                     </div>
                     <div>
                         Data
